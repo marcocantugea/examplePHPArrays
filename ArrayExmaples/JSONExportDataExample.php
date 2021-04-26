231a3b4932244bfa9b26c9315347c2682c118e46
Marco Cantu Gea <marco.cantu.g@gmail.com>
2021-04-25 21:04:46 -0500

    modificar funcion de exportar

--- ArrayExmaples/JSONExportDataExample.php
+++ ArrayExmaples/JSONExportDataExample.php
@@ -2,19 +2,17 @@
 
 
 
-function exportListaUsuarios(array $listaUsuarios):void{
-    file_put_contents("DataExampleUsuarios.json",json_encode($listaUsuarios));
+function exporJson(string $fileName,array $listacilindros):void{
+    file_put_contents($fileName,json_encode($listacilindros));
 }
 
-function exportListaCilindros(array $listacilindros):void{
-    file_put_contents("DataExampleCilindros.json",json_encode($listacilindros));
-
-}
 
 function exportListas(){
     include("DataExample.php");
-    exportListaUsuarios(ListaUsuarios);
-    exportListaCilindros(ListaCilindros);
+    exporJson("DataExampleUsuarios.json",ListaUsuarios);
+    exporJson("DataExampleCilindros.json",ListaCilindros);
+    exporJson("DataExampleCilindrosAveriados.json",ListaCilindrosAveriados);
+    exporJson("DataExampleCilindrosVerificar.json",ListaCilindrosVerificar);
 }
 
 exportListas();
